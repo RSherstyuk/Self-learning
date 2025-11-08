17c0b7d05b373aebd1ae217db60e1a0a50a507eb
RSherstyuk <rus.sherstyuk@gmail.com>
2025-11-08 16:12:49 +0300

    new rules

--- cpp/ten/include/SolutionTwoPointersB.h
+++ cpp/ten/include/SolutionTwoPointersB.h
@@ -1,8 +1,5 @@
 #include "SolutionTwoPointers.h"
 
-#include <vector>
-
-
 class SolutionTwoPointersB : public SolutionTwoPointers {
 public:
     std::vector<std::vector<int>> threeSum(std::vector<int> &nums) override;
