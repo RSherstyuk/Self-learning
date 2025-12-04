eb63bc6c7438f3f0628df6c5f43ca0c3c9ec0c17
RSherstyuk <rus.sherstyuk@gmail.com>
2025-12-04 12:12:35 +0300

    create signature and tests

--- py/ten/src/ten/two_point/two_point.py
+++ py/ten/src/ten/two_point/two_point.py
@@ -11,3 +11,23 @@ def two_point(arr: list[int], target: int) -> list[int]:
             return [arr[l], arr[r]]
 
     return [-1,-1]
+
+def reverse_string(s: str) -> None:
+    l = 0
+    r = len(s) - 1
+    temp = list(s)
+
+    while(l < r):
+        temp[l], temp[r] = temp[r], temp[l]
+        l += 1
+        r -= 1
+
+    list(temp)
+
+def isPoll(s: str) -> bool:
+    ss = s
+    reverse_string(s)
+    return ss == s
+
+def three_sum(arr: list[int]) -> list[int]:
+    return []
--- py/ten/tests/two_test.py
+++ py/ten/tests/two_test.py
@@ -4,3 +4,14 @@ from ten.two_point.two_point import *
 class TestTwoPointTasks:
     def test_two_point(self):
         assert two_point([2, 7, 11, 15], 9) == [2, 7]
+
+    def test_reverse_string(self):
+        s = "gfdsa"
+        reverse_string(s)
+        assert s == "gfdsa"
+
+    def test_is_poll(self):
+        assert isPoll("asddsa") == True
+
+    def test_three_sum(self):
+        assert three_sum([-1, 0, 1, 2, -1, 4]) == [[-1, -1, 2], [-1, 0, 1]]
