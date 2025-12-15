8b3586e98cb1ff0ae8be5dba2c5b5b311b599cd7
RSherstyuk <rus.sherstyuk@gmail.com>
2025-12-15 11:55:26 +0300

    new solution for linkedlist

--- py/ten/src/ten/list/list.py
+++ py/ten/src/ten/list/list.py
@@ -1,4 +1,57 @@
-class List:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
+from __future__ import annotations
+from typing import Optional, Any
+
+
+class Node:
+    def __init__(self, data: Any):
+        self.data: Any = data
+        self.next: Node | None = None
+
+
+class LinkedList:
+    head: Optional[Node]
+
+    def __init__(self):
+        self.head: Node | None = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def insert_at_beginning(self, data: Any) -> None:
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_end(self, data: Any):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        curr: Node = self.head
+
+        while curr.next is not None:
+            curr = curr.next
+
+        curr.next = new_node
+
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(" -> ".join(map(str, elements)))
+
+
+def main():
+    list = LinkedList()
+    list.insert_at_beginning(12)
+    list.insert_at_beginning(12)
+    list.insert_at_beginning(12)
+    list.insert_at_beginning(12)
+    list.display()
+
+
+main()
