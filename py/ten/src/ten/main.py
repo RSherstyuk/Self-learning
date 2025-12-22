a925ebbb210f00b1b7ef040ff03336256980176e
RSherstyuk <rus.sherstyuk@gmail.com>
2025-12-22 13:23:50 +0300

    new fix

--- py/ten/src/ten/main.py
+++ py/ten/src/ten/main.py
@@ -1,6 +1,6 @@
-import numpy as np
-from ten.shad.trash import *
 from typing import Any
+
+import numpy as np
 
 
 def exper(n: int):
@@ -30,12 +30,10 @@ def get_rank(matrix: list[list[Any]]):
         if pivot != -1:
             rank += 1
             selected_rows[pivot] = True
-            # Обнуляем элементы в этом столбце для других строк
             for j in range(rows):
                 if j != pivot:
                     factor = m[j][i] / m[pivot][i]
                     for k in range(i, cols):
-
                         m[j][k] -= factor * m[pivot][k]
     return rank
 
@@ -55,46 +53,41 @@ def get_matrix_rank(matrix):
         if rank == rows:
             break
 
-        # 1. Поиск опорного элемента (пивота) в текущем столбце
-        # Ищем ненулевой элемент ниже текущей строки 'rank'
         pivot_row = -1
         for i in range(rank, rows):
             if abs(m[i][col]) > EPSILON:
                 pivot_row = i
                 break
 
-        # Если в этом столбце все элементы (начиная с rank) равны 0,
-        # то этот столбец не увеличивает ранг. Переходим к следующему.
         if pivot_row == -1:
             continue
 
-        # 2. Меняем местами текущую строку и строку с пивотом
         m[rank], m[pivot_row] = m[pivot_row], m[rank]
 
-        # 3. Обнуляем элементы ниже пивота в текущем столбце
         for i in range(rank + 1, rows):
             if abs(m[i][col]) > EPSILON:
                 factor = m[i][col] / m[rank][col]
-                # Вычитаем из строки i строку rank, умноженную на factor
                 for j in range(col, cols):
                     m[i][j] -= factor * m[rank][j]
 
-        # Переходим к следующей строке, так как текущая уже является базисной
         rank += 1
 
     return rank
+
 
 def missing_number(nums: list[int]) -> int:
     mis = len(nums)
 
     for i in range(len(nums)):
-        mis ^= i ^ nums[i]              
+        mis ^= i ^ nums[i]
 
-    return mis;
+    return mis
+
 
 def main():
-    l = [ord('d') for _ in range(10)]
+    l = [ord("d") for _ in range(10)]
     print(l)
+
 
 if __name__ == "__main__":
     main()
