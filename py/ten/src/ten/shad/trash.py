3cdb979bd39ab094e016a1c15f0e34f7f3a441b7
RSherstyuk <rus.sherstyuk@gmail.com>
2026-01-17 12:13:05 +0300

    find derivate coef

--- py/ten/src/ten/shad/trash.py
+++ py/ten/src/ten/shad/trash.py
@@ -1,40 +1,26 @@
-def spiral(n: int) -> list[list[int]]:
-    if n <= 0:
-        return []
+import numpy as np
 
-    ls = [[0] * n for _ in range(n)]
 
-    top, bottom = 0, n - 1
-    left, right = 0, n - 1
-    current_num = 1
+def find_der(func: list) -> list:
+    res = [0] * len(func)
 
-    while current_num <= n * n:
-        for c in range(left, right + 1):
-            ls[top][c] = current_num
-            current_num += 1
-        top += 1
-        if top > bottom:
-            break
+    for i in range(len(func)):
+        res[i] = func[i] * i
 
-        for r in range(top, bottom + 1):
-            ls[r][right] = current_num
-            current_num += 1
-        right -= 1
-        if left > right:
-            break
+    return res
 
-        for c in range(right, left - 1, -1):
-            ls[bottom][c] = current_num
-            current_num += 1
-        bottom -= 1
-        if top > bottom:
-            break
 
-        for r in range(bottom, top - 1, -1):
-            ls[r][left] = current_num
-            current_num += 1
-        left += 1
-        if left > right:
-            break
+def product_rule_derivative(f_coeffs: list, g_coeffs: list) -> list:
+    der_f = find_der(f_coeffs)
+    der_g = find_der(g_coeffs)
 
-    return ls
+    res = np.polymul(der_f, g_coeffs) + np.polymul(f_coeffs, der_g)
+
+    return list(res)
+
+
+def test():
+    print(product_rule_derivative([2], [3, 4]))
+
+
+test()
