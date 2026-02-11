449354aa09aed9e9a19aa8474620f597b981ac3e
RSherstyuk <rus.sherstyuk@gmail.com>
2026-02-11 19:51:33 +0300

    feat: add tests for hash table

---
+++ py/ten/tests/test_hash_table.py
@@ -0,0 +1,20 @@
+from ten.hash_table.my_hash_map import HashTable
+import pytest
+
+
+class TestHashTable:
+    def test_put_none_key(self):
+        ht = HashTable()
+        with pytest.raises(ValueError):
+            ht.put(None, 21)
+
+    def test_put_op(self):
+        ht = HashTable()
+        ht.put(12,21)
+        assert ht.get(12) == 21
+
+    def test_delete_op(self):
+        ht = HashTable()
+        ht.put(12,21)
+        ht.delete(12)
+        assert ht.get(12) == None
