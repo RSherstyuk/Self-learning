d39a1f4d876196d890af5b11a462b43a6ed265e5
RSherstyuk <rus.sherstyuk@gmail.com>
2026-02-13 15:40:59 +0300

    fix some last code

--- py/ten/src/ten/two_point/two_point.py
+++ py/ten/src/ten/two_point/two_point.py
@@ -1,14 +1,9 @@
-def two_point(arr: list[int], target: int) -> list[int]:
-    l = 0
-    r = len(arr) - 1
-    while l < r:
-        cur_sum = arr[l] + arr[r]
-        if cur_sum < target:
-            l += 1
-        elif cur_sum > target:
-            r -= 1
-        else:
-            return [arr[l], arr[r]]
+def two_point(nums: list[int], target: int) -> list[int]:
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in nums[i + 1 :]:
+            j = nums.index(complement)
+            return [i, j]
 
     return [-1, -1]
 
--- py/ten/tests/two_test.py
+++ py/ten/tests/two_test.py
@@ -3,7 +3,7 @@ from ten.two_point.two_point import *
 
 class TestTwoPointTasks:
     def test_two_point(self):
-        assert two_point([2, 7, 11, 15], 9) == [2, 7]
+        assert two_point([2, 7, 11, 15], 9) == [0, 1]
 
     def test_reverse_string(self):
         s = "gfdsa"
